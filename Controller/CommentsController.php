b1859783af96d60ccfc8bcce58a892339f52241a
shoxxdj <remi.martin.deabia@gmail.com>
2014-05-18 11:58:25 +0200

    Add anti-xss function
    Add an anti-xss function to the plugin

--- Controller/CommentsController.php
+++ Controller/CommentsController.php
@@ -12,6 +12,41 @@ class CommentsController extends AppController{
 	}
 
 	function add(){
+		function xss_clean($data)
+		{
+			// Fix &entity\n;
+			$data = str_replace(array('&amp;','&lt;','&gt;'), array('&amp;amp;','&amp;lt;','&amp;gt;'), $data);
+			$data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
+			$data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
+			$data = html_entity_decode($data, ENT_COMPAT, 'UTF-8');
+	
+			// Remove any attribute starting with "on" or xmlns
+			$data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
+	
+			// Remove javascript: and vbscript: protocols
+			$data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
+			$data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
+			$data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
+	
+			// Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
+			$data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+			$data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+			$data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
+	
+			// Remove namespaced elements (we do not need them)
+			$data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
+	
+			do
+			{
+				// Remove really unwanted tags
+				$old_data = $data;
+				$data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
+			}
+			while ($old_data !== $data);
+	
+			// we are done...
+			return $data;
+		}
 		$referer = $this->referer().'#commentForm';
 		if(!empty($this->request->data)){
 			$user_id = $this->Auth->user("id");
@@ -19,6 +54,9 @@ class CommentsController extends AppController{
 			// We add new datas
 			$this->request->data['Comment']['ip'] = $this->getIp();
 			$this->request->data['Comment']['user_id'] = $user_id ? $user_id : 0;
+				
+			//Clean the input
+			$this->request->data['Comment']['content'] = xss_clean($this->request->data['Comment']['content']);
 
 			$this->Comment->create($this->request->data, true);
 
